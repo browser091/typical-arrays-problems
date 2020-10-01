056cd0e9f8d58775ca55f2b247144e0473145032
AUKS Urban_PS <Urban_PS@mcknew.beltelecom.by>
2020-10-01 13:34:01 +0300

    feat: add completed tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array != undefined && array != '') {
+    return Math.min.apply(null, array)
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+
+exports.max = function max(array) {
+  if (array != undefined && array != '') {
+    return Math.max.apply(null, array)
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array != undefined && array != '') {
+    let summ = 0
+    for (let i = 0; i < array.length; i++) {
+      summ += array[i]
+
+    }
+    return summ / array.length
+  }
   return 0;
 }
